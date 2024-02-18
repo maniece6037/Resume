fa8bf74eb148f6a7d665fdd7a434b35c336c9f12
maniece6037 <160435709+maniece6037@users.noreply.github.com>
2024-02-18 22:15:18 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+  strings: ["Mainframe Developer", "Senior Software Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+// $(document).ready(function () {
+//   $(".menu-toggle").click(function () {
+//     $(".menu-links").slideToggle();
+//   });
+// });
